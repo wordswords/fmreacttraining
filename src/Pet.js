72294f2f9d25a5aac720a02378ff9d1b8a0701b9
David Craddock <contact@davidcraddock.net>
2020-12-08 15:40:54 +0000

    JSX-ified version of tutorial

--- src/Pet.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("ol", {}, [
-    React.createElement("li", { className: "pet" }, name),
-    React.createElement("li", { className: "pet" }, animal),
-    React.createElement("li", { className: "pet" }, breed),
-  ]);
-}
